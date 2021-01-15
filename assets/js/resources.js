4a4ec6a8de6757248ae678623f1dc7f1fb705638
sarad <saradgajurel@gmail.com>
2021-01-15 11:46:31 +0545

    implemented adblock

--- assets/js/resources.js
+++ assets/js/resources.js
@@ -28,3 +28,25 @@ layout: compress
     
 {% endcapture %}
 var resources = {{ resources }};
+window.onload = function() {
+    setTimeout(function() {
+      if ( typeof(window.google_jobrunner) === "undefined" ) {
+       document.getElementById('container').innerHTML=`
+<section class="blog-area bg-color3">
+    <div class="blog-wrapper section-spacing">
+        <div class="container">
+            <div class="error-page text-center">
+            <p style="font-size:64px;color:red;"><i class="fa fa-exclamation-circle"></i></p>
+                <p>It seems like you have AdBlock enabled.</p>
+                <p>We get it! Ads are annoying but they help keep this website up and running. <br/>
+                Please consider disabling your ad block software or whitelist our domain so only our ads are displayed.</p>
+            </div>
+        </div>
+    </div>
+</section>
+       `;
+       document.getElementById('title').scrollIntoView();
+       document.body.style = 'overflow:visible;';
+      }
+    }, 10000);
+  };
